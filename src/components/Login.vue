f4028451179aa7db5ba0688f12fb53b23c1451c5
Hakim Cassimally <hakim.cassimally@couchbase.com>
2021-04-23 15:29:19 +0100

    Update docs links to include Ruby and Scala SDKs
    (NB: each of the target pages will need to be updated once
    the travel app is ported)

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -31,23 +31,21 @@
         </div>
       </div>
       <!-- Info column -->
-      <div class="col-sm">
+      <div class="col-sm container">
         <h1> {{ this.tenantInfo.name }} <img :src="this.tenantInfo.logo" alt="Logo"></h1>
         <p>This is a sample app to demonstrate some of the things Couchbase can do. Create an account, book flights and search hotels while the app displays what's going on behind the scenes.</p>
-        <p>You can find documentation for this app <a href="https://docs.couchbase.com/server/4.5/travel-app/travel-app-walkthough.html" target="_blank">here</a>, and docs for each SDK backend below:</p>
+        <p>You can find documentation for this app at the appropriate page for your preferred SDK:</p>
 
-        <table class="table">
-          <tr>
-            <td class="text-center"><a href="https://docs.couchbase.com/java-sdk/current/sample-app-backend.html" target="_blank">Java</a></td>
-            <td class="text-center"><a href="https://docs.couchbase.com/python-sdk/current/sample-app-backend.html" target="_blank">Python</a></td>
-            <td class="text-center"><a href="https://docs.couchbase.com/nodejs-sdk/current/sample-app-backend.html" target="_blank">Javascript</a></td>
-          </tr>
-          <tr>
-            <td class="text-center"><a href="https://docs.couchbase.com/dotnet-sdk/current/sample-app-backend.html" target="_blank">.NET</a></td>
-            <td class="text-center"><a href="https://docs.couchbase.com/go-sdk/current/sample-app-backend.html" target="_blank">Go</a></td>
-            <td class="text-center"><a href="https://docs.couchbase.com/php-sdk/current/sample-app-backend.html" target="_blank">PHP</a></td>
-          </tr>
-        </table>
+        <ul class="row list-unstyled">
+          <li class="col-4 text-center p-2"><a href="https://docs.couchbase.com/python-sdk/current/sample-app-backend.html" target="_blank">Python</a></li>
+          <li class="col-4 text-center p-2"><a href="https://docs.couchbase.com/java-sdk/current/sample-app-backend.html" target="_blank">Java</a></li>
+          <li class="col-4 text-center p-2"><a href="https://docs.couchbase.com/nodejs-sdk/current/sample-app-backend.html" target="_blank">Javascript</a></li>
+          <li class="col-4 text-center p-2"><a href="https://docs.couchbase.com/dotnet-sdk/current/sample-app-backend.html" target="_blank">.NET</a></li>
+          <li class="col-4 text-center p-2"><a href="https://docs.couchbase.com/go-sdk/current/sample-app-backend.html" target="_blank">Go</a></li>
+          <li class="col-4 text-center p-2"><a href="https://docs.couchbase.com/php-sdk/current/sample-app-backend.html" target="_blank">PHP</a></li>
+          <li class="col-4 text-center p-2"><a href="https://docs.couchbase.com/scala-sdk/current/sample-app-backend.html" target="_blank">Scala</a></li>
+          <li class="col-4 text-center p-2"><a href="https://docs.couchbase.com/ruby-sdk/current/sample-app-backend.html" target="_blank">Ruby</a></li>
+        </ul>
 
         <!-- <div class="row mt-auto mx-auto text-center h-auto">
           <div class="col">
